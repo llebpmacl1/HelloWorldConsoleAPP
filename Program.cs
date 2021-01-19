4aa9ade83d52e675f0df8cb1df727b66c2620c1b
LC <llebpmacl1@gmail.com>
2021-01-19 15:52:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldConsoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
